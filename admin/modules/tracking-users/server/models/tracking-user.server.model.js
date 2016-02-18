0626512b15a9b83623b575c53fc593d54872911d
gaspar belandria <fdirinot@hotmail.com>
2016-02-17 21:44:26 -0500

    user model updated

--- admin/modules/tracking-users/server/models/tracking-user.server.model.js
+++ admin/modules/tracking-users/server/models/tracking-user.server.model.js
@@ -45,11 +45,73 @@ var TrackingUserSchema = new Schema({
 			trim: true
 		}
 	},
-	name: {
+	firstname: {
 		type: String,
 		default: '',
-		required: 'Please fill Tracking user name',
+		required: 'Please fill Tracking user firstname',
 		trim: true
+	},
+	lastname: {
+		type: String,
+		default: '',
+		required: 'Please fill Tracking user lastname',
+		trim: true
+	},
+	email: {
+		type: String,
+		default: '',
+		required: 'Please fill Tracking user email',
+		trim: true
+	},
+	phone: {
+		type: String,
+		default: '',
+		required: 'Please fill Tracking user phone',
+		trim: true
+	},
+	cellphone: {
+		type: String,
+		default: '',
+		required: 'Please fill Tracking user firstname',
+		trim: true
+	},
+	fax: {
+		type: String,
+		default: '',
+		required: 'Please fill Tracking user fax',
+		trim: true
+	},
+	address:{
+		street: {
+			type: String,
+			default: '',
+			required: 'Please fill Tracking user street',
+			trim: true
+		},
+		city: {
+			type: String,
+			default: '',
+			required: 'Please fill Tracking user city',
+			trim: true
+		},
+		state: {
+			type: String,
+			default: '',
+			required: 'Please fill Tracking user state',
+			trim: true
+		},
+		zip: {
+			type: String,
+			default: '',
+			required: 'Please fill Tracking user zip',
+			trim: true
+		},
+		country: {
+			type: String,
+			default: '',
+			required: 'Please fill Tracking user country',
+			trim: true
+		}
 	}
 });
 
